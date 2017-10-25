b384817a10c48cca3438e0914747a14518ebde09
EVA LASTERS <EVA.LASTERS@STUDENT.AP.BE>
2017-10-26 00:27:51 +0200

    Extra info

--- _harp/assets/javascript/script.js
+++ _harp/assets/javascript/script.js
@@ -2,6 +2,18 @@ function main() {
       jQuery(document).ready(function() {
           jQuery(".external-link").attr("target", "_blank");
       });
+      var elem = document.querySelector('.grid');
+      var iso = new Isotope( elem, {
+        // options
+        itemSelector: '.grid-item',
+        layoutMode: 'fitRows'
+      });
+
+      // element argument can be a selector string
+      //   for an individual element
+      var iso = new Isotope( '.grid', {
+        // options
+      });
 }
 
 window.onload = function() {
