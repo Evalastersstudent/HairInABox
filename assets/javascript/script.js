e1dc1716587cd3ae546e2cce1893a107efac8426
EVA LASTERS <EVA.LASTERS@STUDENT.AP.BE>
2017-10-26 01:51:39 +0200

    Message In A Box

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,15 +3,6 @@ function main() {
             jQuery(".external-link").attr("target", "_blank");
       });
 
-      $('.grid').isotope({
-            layoutMode: 'cellsByRow',
-            itemSelector: '.grid-item',
-            cellsByRow: {
-                  columnWidth: 220,
-                  rowHeight: 220
-            }
-      });
-
       $(document).ready(function() {
             $('#navigatie').localScroll({duration: 500});
       });
