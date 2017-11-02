ae8263f3fc1f3155a4b3b7c526a2af65495a32ef
EVA LASTERS <EVA.LASTERS@STUDENT.AP.BE>
2017-11-02 01:04:28 +0100

    Message In A Box

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,20 @@
 function main() {
-      jQuery(document).ready(function() {
-            jQuery(".external-link").attr("target", "_blank");
-      });
-
       $(document).ready(function() {
             $('#navigatie').localScroll({speed: 500});
       });
+
+      'use strict';
+      (function() {
+        var body = document.body;
+        var menu = document.getElementsByClassName('menu')[0];
+        var navkleinscherm = document.getElementsByClassName('navkleinscherm')[0];
+
+        menu.addEventListener('click', function toggleClasses() {
+          [body, menu, navkleinscherm].forEach(function (el) {
+            el.classList.toggle('open');
+          });
+        }, false);
+      })();
 
       $(document).ready(function() {
           $('#contact_name').on('input', function() {
